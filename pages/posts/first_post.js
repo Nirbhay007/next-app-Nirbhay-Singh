360b2ef920c1a58b804537de91160826888acd66
Nirbhay Singh <nirbhaysinghbe1@gmail.com>
2021-10-11 22:38:55 +0530

    done with the styling and home page

--- pages/posts/first_post.js
+++ pages/posts/first_post.js
@@ -1,11 +1,19 @@
 import Link from "next/link";
-
+import Head from "next/head";
+import Layout from "../../components/layout";
 export default function FirstPost() {
   return (
-    <h1>
-      <Link href="/">
-        <a>Go to root page </a>
-      </Link>
-    </h1>
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
+      <h1>My first post </h1>
+      <h2>
+        <Link href="/">
+          <a>Go to root page </a>
+        </Link>
+      </h2>
+    </>
   );
 }
